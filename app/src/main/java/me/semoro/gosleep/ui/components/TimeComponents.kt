c1871332aa39be19f0735a3f1ac00ee1ef939d81
Simon.Ogorodnik <simon.ogorodnik@gmail.com>
2025-04-12 10:11:39 +0200

    Remove duplicated code in TimeComponents

--- app/src/main/java/me/semoro/gosleep/ui/components/TimeComponents.kt
+++ app/src/main/java/me/semoro/gosleep/ui/components/TimeComponents.kt
@@ -2,19 +2,16 @@ package me.semoro.gosleep.ui.components
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.material3.TimePicker
-import androidx.compose.material3.rememberTimePickerState
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlinx.datetime.LocalTime
 import kotlinx.datetime.toJavaLocalTime
 import me.semoro.gosleep.data.UserSettings
 import java.time.format.DateTimeFormatter
-
 import kotlin.time.Duration.Companion.minutes
 
 @Composable
@@ -110,12 +107,12 @@ fun SleepSettingsDialog(
         mutableStateOf(minutes)
     }
 
-    var greenZoneDurationSlider by remember {
+    var greenZoneDurationSlider = remember {
         val minutes = settings.greenZoneDuration.inWholeMinutes.toFloat()
         mutableStateOf(minutes)
     }
 
-    var yellowZoneDurationSlider by remember {
+    var yellowZoneDurationSlider = remember {
         val minutes = settings.yellowZoneDuration.inWholeMinutes.toFloat()
         mutableStateOf(minutes)
     }
@@ -163,73 +160,9 @@ fun SleepSettingsDialog(
                     )
                 }
 
-                // Green Zone Duration Section
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.spacedBy(8.dp)
-                ) {
-                    Text(
-                        text = "Green Zone Duration",
-                        style = MaterialTheme.typography.titleMedium
-                    )
 
-                    // Display the selected duration
-                    val greenZoneDurationInMinutes = greenZoneDurationSlider.toInt().minutes
-                    val greenZoneDurationText = greenZoneDurationInMinutes.toString()
-
-                    Text(
-                        text = greenZoneDurationText,
-                        style = MaterialTheme.typography.bodyMedium
-                    )
-
-                    // Slider for selecting yellow zone duration
-                    Slider(
-                        value = greenZoneDurationSlider,
-                        onValueChange = { newValue ->
-                            greenZoneDurationSlider = newValue
-                        },
-                        valueRange = 10f..60f,
-                        steps = 4,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 16.dp)
-                    )
-                }
-
-                // Yellow Zone Duration Section
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.spacedBy(8.dp)
-                ) {
-                    Text(
-                        text = "Yellow Zone Duration",
-                        style = MaterialTheme.typography.titleMedium
-                    )
-
-                    // Display the selected duration
-                    val yellowZoneDurationInMinutes = yellowZoneDurationSlider.toInt().minutes
-                    val yellowZoneDurationText = yellowZoneDurationInMinutes.toString()
-
-                    Text(
-                        text = yellowZoneDurationText,
-                        style = MaterialTheme.typography.bodyMedium
-                    )
-
-                    // Slider for selecting red zone duration
-                    Slider(
-                        value = yellowZoneDurationSlider,
-                        onValueChange = { newValue ->
-                            yellowZoneDurationSlider = newValue
-                        },
-                        valueRange = 10f..60f,
-                        steps = 4,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 16.dp)
-                    )
-                }
+                ZoneDurationSlider(greenZoneDurationSlider, "Green Zone Duration")
+                ZoneDurationSlider(yellowZoneDurationSlider, "Yellow Zone Duration")
             }
         },
         confirmButton = {
@@ -237,8 +170,8 @@ fun SleepSettingsDialog(
                 onClick = {
                     onChangeSettings(settings.copy(
                         desiredSleepDuration = sleepDurationSliderPosition.toInt().minutes,
-                        greenZoneDuration = greenZoneDurationSlider.toInt().minutes,
-                        yellowZoneDuration = yellowZoneDurationSlider.toInt().minutes
+                        greenZoneDuration = greenZoneDurationSlider.value.toInt().minutes,
+                        yellowZoneDuration = yellowZoneDurationSlider.value.toInt().minutes
                     ))
                     closeDialog()
                 }
@@ -254,6 +187,46 @@ fun SleepSettingsDialog(
             }
         }
     )
+}
+
+@Composable
+private fun ZoneDurationSlider(
+    state: MutableState<Float>,
+    text: String,
+) {
+    // Green Zone Duration Section
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.titleMedium
+        )
+
+        // Display the selected duration
+        val greenZoneDurationInMinutes = state.value.toInt().minutes
+        val greenZoneDurationText = greenZoneDurationInMinutes.toString()
+
+        Text(
+            text = greenZoneDurationText,
+            style = MaterialTheme.typography.bodyMedium
+        )
+
+        // Slider for selecting yellow zone duration
+        Slider(
+            value = state.value,
+            onValueChange = { newValue ->
+                state.value = newValue
+            },
+            valueRange = 10f..60f,
+            steps = 4,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        )
+    }
 }
 
 @Composable
@@ -289,4 +262,17 @@ fun SleepDurationSettingsChip(
             closeDialog = { showDialog = false }
         )
     }
+}
+
+
+@Preview
+@Composable
+fun SleepSettingsDialogPreview() {
+    var settings by remember { mutableStateOf(UserSettings()) }
+
+    SleepSettingsDialog(settings, {
+        settings = it
+    }, {
+        // do nothing
+    })
 }
