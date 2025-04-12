f7c8b69471c9380762eca2ca6c28e981e0572eca
Simon.Ogorodnik <simon.ogorodnik@gmail.com>
2025-04-12 10:10:24 +0200

    Add debug ProductConfig

---
+++ app/src/debug/kotlin/ProductConfig.kt
@@ -0,0 +1,10 @@
+package me.semoro.gosleep
+
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
+
+const val IS_RELEASE = false
+const val IS_DEBUG = true
+val GREEN_BEEP_INTERVAL = 15.seconds
+val YELLOW_BEEP_INTERVAL = 15.seconds
+val RED_BEEP_INTERVAL = 15.seconds
